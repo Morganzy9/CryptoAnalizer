579f856a6e550b514c217e6ec9c669a6284927fb
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-06-26 17:26:31 +0500

    - Updated UI to get data from another API call

--- CryptoAnalizer/Conttollers/DetailsVC.swift
+++ CryptoAnalizer/Conttollers/DetailsVC.swift
@@ -18,36 +18,25 @@ class DetailsVC: UIViewController {
     var coinDetailed: CryptoDetails?
     
     //    MARK: - UILifeCycle
-
+    
     override func loadView() {
         super.loadView()
-
+        
         cryptoDetailsView = CryptoDetailsView()
-//        cryptoDetailsView.symbolLabel.text = "Symbol of cion: \(coinDetailed?.name)"
-//        cryptoDetailsView.currPriceLabel.text = "Current Price: \(String(selectedCoin.currentPrice))"
-//        cryptoDetailsView.high24Label.text = "High 24 hours: \(String(selectedCoin.high24h))"
-        
-        guard let currentPrice = cryptoDetailsView.currPriceLabel.text else { return }
-        guard let high24 = cryptoDetailsView.high24Label.text else { return }
-        
-//        cryptoDetailsView.currPriceLabel.changePartOfTextColor(fullText: currentPrice, changeText: String(selectedCoin.currentPrice))
-//        cryptoDetailsView.high24Label.changePartOfTextColor(fullText: high24, changeText: String(selectedCoin.high24h))
         
         view = cryptoDetailsView
         
     }
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setUpView()
         
-        print(coinDetailed?.name)
-        
     }
     
-
+    
     //    MARK: - FUnction`s
     
     func setUpView() {
@@ -57,20 +46,44 @@ class DetailsVC: UIViewController {
         let cryptoApi = CryptoAPI()
         
         cryptoApi.fetchCryptoInDetails(selectedCoin: selectedCoin) { [weak self] crypto in
-            
-            
-            print(self?.coinDetailed?.name)
-            print(crypto?.name)
+            guard let self = self else { return }
             
             DispatchQueue.main.async {
-                self?.coinDetailed = crypto
-                self?.loadView()
+                self.coinDetailed = crypto
+                self.updateLabels()
             }
-            
         }
-
+        
         
     }
     
-
+    func updateLabels() {
+        
+        guard let name = coinDetailed?.name else {
+            
+            print("Error in label name")
+            
+            return
+        }
+        
+        guard let id = coinDetailed?.id else {
+            
+            print("Error in label id")
+            
+            return
+        }
+        
+        guard let symbol = coinDetailed?.symbol else {
+            
+            print("Error in label symbol")
+            
+            return
+        }
+        
+        cryptoDetailsView.symbolLabel.text = "Symbol of coin: \(name)"
+        cryptoDetailsView.currPriceLabel.text = "Current Price: \(id)"
+        cryptoDetailsView.high24Label.text = "High 24 hours: \(symbol)"
+    }
+    
+    
 }
